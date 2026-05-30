8413386931bf0af430cbd7a624a49c2131834855
Mohammed <abdullahqb57@gmail.com>
2026-05-30 15:04:56 +0530

    login setup

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,21 +1,63 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {useMutation} from '@tanstack/react-query'
+import axios from 'axios'
 import '../styles/login.css'
 
 const Login = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const handleLogin = async ({email, password}) => {
+    try {
+      const {data} = await axios.post('http://localhost:3000/login', {email, password}, { withCredentials: true});
+      return data;
+    } catch (error) {
+      console.error('Error during login:', error);
+      throw error; // Rethrow the error to be caught by onError in useMutation
+    }
+    
+  }
+  const loginMutation = useMutation({
+    mutationFn: handleLogin,
+    onSuccess: (data) => {
+      console.log('Login successful:', data);
+    },
+    onError: (error) => {
+      console.error('Login failed:', error);
+    },
+  });
+
+  const onSubmit = () => {
+    loginMutation.mutate({email, password});
+  }
   return (
     <div className='login'>
         <h2 className='login-title'>Login Page</h2>
             <div>
                 <span>Email ID</span>
-                <input type="text" id="username" name="username" className='login-input' />
+                <input 
+                    type="text" 
+                    id="username" 
+                    name="username" 
+                    className='login-input' 
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
             </div>
             <div>
                 <span>Password</span>
-                <input type="password" id="password" name="password" className='login-input' />
+                <input 
+                    type="password" 
+                    id="password" 
+                    name="password" 
+                    className='login-input' 
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                /> 
             </div>
-            <button className='login-button'>Login</button>
+            <button className='login-button' onClick = {onSubmit}>Login</button>
     </div>
   )
 }
 
-export default Login
+export default Login;
